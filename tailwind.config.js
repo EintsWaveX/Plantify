4d803c0470bf2576ac9aefc9bd3560ad40d36b48
EintsWaveX <nue.immanuel@gmail.com>
2024-08-30 00:32:01 +0800

    Renew the Tailwind CSS framework for better building time.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      screens: {
+          screen_s: "340px",
+          screen_m: "540px",
+          screen_l: "768px",
+          screen_xl: "1180px",
+        },
+        extend: {}
     },
     plugins: [],
 }
-
